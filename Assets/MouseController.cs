9f7eefe551a32ded8d89bd72b3575eb20379f4cc
David Giasson <david.giasson@outlook.com>
2022-04-27 16:07:49 -0400

    Filtering clicks based on layers and tags.

--- Assets/MouseController.cs
+++ Assets/MouseController.cs
@@ -6,6 +6,10 @@ public class MouseController : MonoBehaviour
 {
     private const int LeftClick = 0, RightClick = 1;
 
+    public LayerMask layerMask;
+
+    private GameObject ActivePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,26 @@ public class MouseController : MonoBehaviour
         if (Input.GetMouseButtonDown(LeftClick))
         {
             var target = GetTarget();
+            if (target != null && target.gameObject.layer != LayerMask.NameToLayer("Inactive"))
+            {
+                switch (target.tag)
+                {
+                    case "Player":
+                        ActivePlayer = target.gameObject;
+                        Debug.Log(ActivePlayer.name);
+                        break;
+                    case "Gate":
+                        Debug.Log("Gate");
+                        break;
+                    case "Key":
+                        Debug.Log("Key");
+                        break;
+                    default:
+                        Debug.Log($"Unknown target: {target.name} ({target.tag})");
+                        break;
+                }
+                Debug.Log(target?.name ?? "null");
+            }
         }
     }
 
